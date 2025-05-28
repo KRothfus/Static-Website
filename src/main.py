a948df74027a243e53158ede2de0f1bee4360db6
KRothfus <krothfus92@gmail.com>
2025-05-28 05:58:44 -0700

    deleted public and created docs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import os
 import shutil
+import sys
 
 from pagegeneration import generate_pages_recursive
+
+
 
 def copy_files_to_public():
     def recursive_to_public(initial_dir = './static'):
@@ -9,22 +12,24 @@ def copy_files_to_public():
             dirs = os.listdir(initial_dir)
             for dir in dirs:
                 if os.path.isfile(initial_dir +'/'+dir):
-                    print(f'Copying file: {initial_dir.replace('static','public')+'/'+dir}')
-                    shutil.copy(initial_dir+'/'+dir,initial_dir.replace('static','public')+'/'+dir)
+                    print(f'Copying file: {initial_dir.replace('static','docs')+'/'+dir}')
+                    shutil.copy(initial_dir+'/'+dir,initial_dir.replace('static','docs')+'/'+dir)
                 else:
-                    print(f'Making directory: {initial_dir.replace('static','public')+'/'+dir}')
-                    os.mkdir(initial_dir.replace('static','public')+'/'+dir)
+                    print(f'Making directory: {initial_dir.replace('static','docs')+'/'+dir}')
+                    os.mkdir(initial_dir.replace('static','docs')+'/'+dir)
                     recursive_to_public(initial_dir+'/'+dir)
-    if os.path.exists('./public'):
-        print(f'Removing Public Directory')
-        shutil.rmtree('./public')
-    print('Creating Public Directory')
-    os.mkdir('./public')
+    if os.path.exists('./docs'):
+        print(f'Removing docs Directory')
+        shutil.rmtree('./docs')
+    print('Creating docs Directory')
+    os.mkdir('./docs')
     recursive_to_public()
 
 def main():
+    basepath = sys.argv
+    print(basepath)
     copy_files_to_public()
-    generate_pages_recursive('content','template.html','public')
+    generate_pages_recursive('content','template.html','docs',basepath)
 # result = TextNode('this is some anchor text','link','https://www.boot.dev')
 # print(result)
 
--- src/pagegeneration.py
+++ src/pagegeneration.py
@@ -3,7 +3,7 @@ import os
 from buildhtml import extract_title, markdown_to_html_node
 
 
-def generate_page(from_path, template_path,dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
     with open(from_path) as f:
         markdown = f.read()
@@ -14,19 +14,19 @@ def generate_page(from_path, template_path,dest_path):
     title = extract_title(markdown)
     template = template.replace('{{ Title }}',title)
     template = template.replace('{{ Content }}', markdown_html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    # if not os.path.exists(dest_path):
-    #     os.makedirs(dest_path)
     with open(dest_path, mode='w') as d:
         d.write(template)
         
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     try:
         dir_list = os.listdir(dir_path_content)
     except:
         raise Exception('nothing to parse')
     for dir in dir_list:
         if os.path.isfile(os.path.join(dir_path_content,dir)):
-            generate_page(os.path.join(dir_path_content,dir), template_path, os.path.join(dest_dir_path,dir.replace('.md','.html')))
+            generate_page(os.path.join(dir_path_content,dir), template_path, os.path.join(dest_dir_path,dir.replace('.md','.html')),basepath)
         else:
-            generate_pages_recursive(os.path.join(dir_path_content,dir), template_path,os.path.join(dest_dir_path,dir))
+            generate_pages_recursive(os.path.join(dir_path_content,dir), template_path,os.path.join(dest_dir_path,dir), basepath)
